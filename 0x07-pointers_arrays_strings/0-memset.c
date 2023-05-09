7fa7e7d35a6f7e88920aafabeb8cc67f8bccf299
EfyaStardom1 <asomahchristabel2@gmail.com>
2023-05-08 21:00:40 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,16 +5,15 @@
  * @s: starting address of memory to be filled
  * @b: the desired value
  * @n: number of bytes to be changed
- * Return: change array with a new value for n bytes
+ * Return: Always 0 (Success)
  */
 char *_memset(char *s, char b, unsigned int n);
 {
-	int i = 0;
+	unsigned int i = 0;
 
-	for (; n > 0; i++)
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
-		n--;
 	}
 	return (s);
 }
--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -4,7 +4,7 @@
 /** 
  * _putchar - writes the character c to stdout
  * @c: The character to print
- * Return: 1 (Success)
+ * Return: on success 1
  * On erro, -1 is returned, and errno is set appropriately
  */
 int _putchar(char c)
